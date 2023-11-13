4af299c6d1bc7bfa916ea8eb53605d4320200c1f
Nando PL <nandopiless@gmail.com>
2023-11-13 18:40:26 +0100

    Checkboxes created with relation to the actions that will do

--- App/main.js
+++ App/main.js
@@ -1,5 +1,7 @@
 const minValueRange = 7;
 const maxValueRange = 33;
+const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+let checkboxesChecked = ['lowercase-check'];
 
 
 
@@ -19,3 +21,28 @@ setInitialRangeValues();
 document.getElementById('num-characts').addEventListener('mousemove', () => {
     document.getElementById('number-characts-selected').textContent = document.getElementById('num-characts').value;
 });
+
+const actions = {
+    'lowercase-check': () => console.log('lower'),
+    'uppercase-check': () => console.log('upper'),
+    'numbers-check': () => console.log('numbers'),
+    'symbols-check': () => console.log('symbols')
+};
+
+
+
+checkboxes.forEach((checkbox) => {
+    checkbox.addEventListener('click', () => {
+        checkboxesChecked = [];
+        checkboxes.forEach((checkbox) => {
+            if (checkbox.checked) {
+                checkboxesChecked.push(checkbox.id);
+            }
+        })
+    })
+})
+document.getElementById('generate-btn').addEventListener('click', () => {
+    checkboxesChecked.forEach((checkbox) => {
+        actions[checkbox]();
+    })
+})
