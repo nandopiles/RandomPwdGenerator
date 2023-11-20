8ceba9ff52ed2044902d7b5469440d4f5bacf668
Nando PL <nandopiless@gmail.com>
2023-11-20 23:02:29 +0100

    Delete action finished

--- App/ListPasswords.js
+++ App/ListPasswords.js
@@ -1,3 +1,15 @@
+const deletePassword = (listBtn) => {
+    listBtn.forEach((btn, index) => {
+        btn.addEventListener('click', () => {
+            const passwordList = JSON.parse(localStorage.getItem('passwordList')) || [];
+
+            passwordList.splice(index, 1);
+            localStorage.setItem('passwordList', JSON.stringify(passwordList));
+            loadPasswords();
+        });
+    });
+};
+
 const loadPasswords = () => {
     const passwordList = JSON.parse(localStorage.getItem('passwordList')) || [];
 
@@ -8,9 +20,11 @@ const loadPasswords = () => {
             <td>${element.emailValue}</td>
             <td>${element.linkValue}</td>
             <td>${element.passwordValue}</td>
+            <td><button type="button" class="btn btn-danger" name="delete-btn">X</button></td>
         </tr>`
         document.getElementById('password-table-body').innerHTML += row;
     });
+    deletePassword(document.getElementsByName('delete-btn'));
 };
 
 
