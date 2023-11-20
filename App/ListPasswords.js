2e6ad567f4198bd535869800b27e2b626325551d
Nando PL <nandopiless@gmail.com>
2023-11-20 22:45:06 +0100

    Table displayed without filtering with the searchbox

--- App/ListPasswords.js
+++ App/ListPasswords.js
@@ -1,1 +1,19 @@
+const loadPasswords = () => {
+    const passwordList = JSON.parse(localStorage.getItem('passwordList')) || [];
+
+    document.getElementById('password-table-body').innerHTML = "";
+    passwordList.forEach(element => {
+        const row = `
+        <tr>
+            <td>${element.emailValue}</td>
+            <td>${element.linkValue}</td>
+            <td>${element.passwordValue}</td>
+        </tr>`
+        document.getElementById('password-table-body').innerHTML += row;
+    });
+};
+
+
+
 document.getElementById('back-btn').addEventListener('click', () => window.location = 'Index.html');
+loadPasswords();
